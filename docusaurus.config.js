ddb769a7f6f744e941a6e6432620e15d08861936
Amith Mohanan <amithm7@gmail.com>
2021-08-14 19:12:31 +0530

    feat: Offline Search
    Using https://github.com/easyops-cn/docusaurus-search-local

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -128,4 +128,15 @@ module.exports = {
       },
     ],
   ],
+  plugins: [
+    [
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      {
+        // `hashed` is recommended as long-term-cache of index file is possible.
+        hashed: true,
+        language: ["en"],
+        indexPages: true,
+      },
+    ],
+  ],
 };
